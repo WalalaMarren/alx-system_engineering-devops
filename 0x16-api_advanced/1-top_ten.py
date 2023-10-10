e48bc649f299441db10f21500d40c3b78ba49bd5
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-10-10 05:05:37 +0000

    api advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+import request
+import sys
+
+
+def top_ten(subreddit):
+    """queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit"""
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': 'u_agent'
+    }
+
+    params = {
+        'limit' = 10
+    }
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit"
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False)
+    if (response.status_code != 200):
+        print(None)
+        return
+    posts = request.json()['data']['children']
+    if len(posts) is 0:
+        print(None)
+    else:
+        print(post['data']['title'])
